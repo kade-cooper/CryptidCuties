b14b28f18a49fc03eed0ce21dbb9c21698e2c277
Lfowler50 <117687794+Lfowler50@users.noreply.github.com>
2023-09-16 18:05:47 -0400

    Begun working on the romance mechanic
    Not full built out or implemented at all yet. Need to get attacks built and working first I feel.

--- Assets/scripts/Movement/NetworkCharacterControllerPrototypeCustom.cs
+++ Assets/scripts/Movement/NetworkCharacterControllerPrototypeCustom.cs
@@ -6,14 +6,26 @@ using UnityEngine;
 [OrderBefore(typeof(NetworkTransform))]
 [DisallowMultipleComponent]
 // ReSharper disable once CheckNamespace
-public class NetworkCharacterControllerPrototypeCustom : NetworkTransform {
+public class NetworkCharacterControllerPrototypeCustom : NetworkTransform
+{
   [Header("Character Controller Settings")]
-  public float gravity       = -20.0f;
-  public float jumpImpulse   = 8.0f;
-  public float acceleration  = 10.0f;
-  public float braking       = 10.0f;
-  public float maxSpeed      = 2.0f;
+  public float gravity = -20.0f;
+  public float jumpImpulse = 8.0f;
+  public float acceleration = 10.0f;
+  public float braking = 10.0f;
+  public float maxSpeed = 2.0f;
   public float rotationSpeed = 15.0f;
+
+  [Header("Romance Mechanic Settings")]
+  public bool isHit;
+  public bool onHit;
+  public float Health = 100.0f;
+  public float RomanceMeter = 0.0f;
+  public float RomanceMeterFull = 5.0f;
+  public bool RomanceReady;
+  public bool isAttacking;
+  public bool attackLands;
+
 
   [Networked]
   [HideInInspector]
@@ -37,25 +49,36 @@ public class NetworkCharacterControllerPrototypeCustom : NetworkTransform {
 
   public CharacterController Controller { get; private set; }
 
-  protected override void Awake() {
+  protected override void Awake()
+  {
     base.Awake();
     CacheController();
+    isHit = false;
+    onHit = false;
+    isAttacking = false;
+    attackLands = false;
+    Romance(isHit, RomanceMeter, RomanceReady);
+    Attack(Health, isAttacking, attackLands);
   }
 
-  public override void Spawned() {
+  public override void Spawned()
+  {
     base.Spawned();
     CacheController();
   }
 
-  private void CacheController() {
-    if (Controller == null) {
+  private void CacheController()
+  {
+    if (Controller == null)
+    {
       Controller = GetComponent<CharacterController>();
 
       Assert.Check(Controller != null, $"An object with {nameof(NetworkCharacterControllerPrototype)} must also have a {nameof(CharacterController)} component.");
     }
   }
 
-  protected override void CopyFromBufferToEngine() {
+  protected override void CopyFromBufferToEngine()
+  {
     // Trick: CC must be disabled before resetting the transform state
     Controller.enabled = false;
 
@@ -71,11 +94,13 @@ public class NetworkCharacterControllerPrototypeCustom : NetworkTransform {
   /// <param name="ignoreGrounded">Jump even if not in a grounded state.</param>
   /// <param name="overrideImpulse">Optional field to override the jump impulse. If null, <see cref="jumpImpulse"/> is used.</param>
   /// </summary>
-  public virtual void Jump(bool ignoreGrounded = false, float? overrideImpulse = null) {
-    if (IsGrounded || ignoreGrounded) {
+  public virtual void Jump(bool ignoreGrounded = false, float? overrideImpulse = null)
+  {
+    if (IsGrounded || ignoreGrounded)
+    {
       var newVel = Velocity;
       newVel.y += overrideImpulse ?? jumpImpulse;
-      Velocity =  newVel;
+      Velocity = newVel;
     }
   }
 
@@ -83,14 +108,16 @@ public class NetworkCharacterControllerPrototypeCustom : NetworkTransform {
   /// Basic implementation of a character controller's movement function based on an intended direction.
   /// <param name="direction">Intended movement direction, subject to movement query, acceleration and max speed values.</param>
   /// </summary>
-  public virtual void Move(Vector3 direction) {
-    var deltaTime    = Runner.DeltaTime;
-    var previousPos  = transform.position;
+  public virtual void Move(Vector3 direction)
+  {
+    var deltaTime = Runner.DeltaTime;
+    var previousPos = transform.position;
     var moveVelocity = Velocity;
 
     direction = direction.normalized;
 
-    if (IsGrounded && moveVelocity.y < 0) {
+    if (IsGrounded && moveVelocity.y < 0)
+    {
       moveVelocity.y = 0f;
     }
 
@@ -100,10 +127,13 @@ public class NetworkCharacterControllerPrototypeCustom : NetworkTransform {
     horizontalVel.x = moveVelocity.x;
     horizontalVel.z = moveVelocity.z;
 
-    if (direction == default) {
+    if (direction == default)
+    {
       horizontalVel = Vector3.Lerp(horizontalVel, default, braking * deltaTime);
-    } else {
-      horizontalVel      = Vector3.ClampMagnitude(horizontalVel + direction * acceleration * deltaTime, maxSpeed);
+    }
+    else
+    {
+      horizontalVel = Vector3.ClampMagnitude(horizontalVel + direction * acceleration * deltaTime, maxSpeed);
       transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Runner.DeltaTime);
     }
 
@@ -112,7 +142,46 @@ public class NetworkCharacterControllerPrototypeCustom : NetworkTransform {
 
     Controller.Move(moveVelocity * deltaTime);
 
-    Velocity   = (transform.position - previousPos) * Runner.Simulation.Config.TickRate;
+    Velocity = (transform.position - previousPos) * Runner.Simulation.Config.TickRate;
     IsGrounded = Controller.isGrounded;
+  }
+  //Here is the setup for when the health of character reaches 0
+  public virtual void DestroyGameObject(float Health)
+  {
+    if (Health == 0.0f)
+    {
+      Destroy(gameObject);
+    }
+  }
+  //Here is the function for the Romance mechanic, currently passing two bools and a float to build-up and initiate the readiness of the Romance/ULT ability.
+  public void Romance(bool isHit, float RomanceMeter, bool RomanceReady)
+  {
+    if (isHit == true)
+    {
+      RomanceMeter += 1.0f;
+    }
+
+    if (RomanceReady == true)
+    {
+      // When attack is setup insert "ULT" Romance attack here.
+    }
+  }
+  //Function for all attacks and how they operate both for the character/player and the enemy/opponent.
+  public void Attack(float Health, bool isAttacking, bool attackLands)
+  {
+    if (isAttacking == true)
+    {
+
+    }
+
+    if (attackLands == true)
+    {
+
+    }
+
+    if (Health == 0.0f)
+    {
+      Destroy(gameObject);
+    }
   }
 }
