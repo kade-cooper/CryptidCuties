e548749eab16923ae796132c6df75b35d95dfa53
Lfowler50 <117687794+Lfowler50@users.noreply.github.com>
2023-10-18 20:45:14 -0400

    Begun work on creating the shooting ability and gearing up to finish the other character abilities.

---
+++ Assets/scripts/ShootingAbility.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+
+[CreateAssetMenu]
+public class ShootingAbility : Ability
+{
+    public GameObject bullet;
+    public float bulletVelocity;
+    public Transform spawnBullet; //may need to change this to rotate point or whatever the actual name is of object being used in inspector.
+    public LayerMask ground;
+
+    public override void Activate(GameObject thisThing)
+    {
+
+    }
+
+}
