c48dce64835f74c215473a9768f9446f9e308710
Lfowler50 <117687794+Lfowler50@users.noreply.github.com>
2023-12-12 15:17:15 -0500

    after merge push with some conflictions on the axescript

--- Assets/AxeScript.cs
+++ Assets/AxeScript.cs
@@ -3,27 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 
-public class AxeScript : NetworkBehaviour
+public class axeScript : NetworkBehaviour
 {
-    [Networked] public int thisLayer { get; set; }
+
+    [Networked] public int thisLayer {get; set;}
+    public int velocity;
+    public int angVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (HasStateAuthority)
-        {
+        if (HasStateAuthority) {
             RPC_SendLayer(this.gameObject.layer);
         }
         this.gameObject.layer = thisLayer;
         StartCoroutine(Retry());
         //this.gameObject.layer = tempLayer;
+        this.gameObject.GetComponent<Rigidbody2D>().angularVelocity = angVelocity;
+        this.gameObject.GetComponent<Rigidbody2D>().velocity = this.transform.right * velocity;
     }
 
     IEnumerator Retry()
     {
         yield return new WaitForSeconds(.1f);
         this.gameObject.layer = thisLayer;
-        this.gameObject.GetComponent<PolygonCollider2D>().enabled = true;
+        this.gameObject.GetComponent<CircleCollider2D>().enabled = true;
 
 
     }
@@ -31,8 +35,15 @@ public class AxeScript : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "wall")
+            Destroy(this.gameObject);
+    }
+    
 
     [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
     public void RPC_SendLayer(int layer)
